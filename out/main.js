b0b3dcc1ca8001be69bea27eeb81456a18659c81
sokintun7339656 <club_1303@mail.ru>
2022-03-03 18:35:48 +0500

    ishkash

--- out/main.js
+++ out/main.js
@@ -1,2 +1,16 @@
 "use strict";
-console.log("salom tupescript");
+// let name = 'Boltaboy'
+Object.defineProperty(exports, "__esModule", { value: true });
+// console.log(name);
+// let dollar: number = 10740
+// console.log(dollar + 740);
+let x = 5;
+let y = 0;
+if (x > 0) {
+    y = Math.cos(x);
+    console.log("y=cos:", y);
+}
+else {
+    y = Math.sin(x);
+    console.log("y=sin:", y);
+}
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,25 @@
+// let name = 'Boltaboy'
 
-console.log("salom tupescript");
+// console.log(name);
+
+// let dollar: number = 10740
+// console.log(dollar + 740);
+let x: number = 5
+let y: number = 0
+if (x > 0) {
+  y = Math.cos(x)
+  console.log("y=cos:" , y);
+  
+}
+else{
+  y = Math.sin(x)
+  console.log("y=sin:" , y);
+}
+
+
+// console.log('y=', y);
+
+
+
+
+export{}
