716fefc4441a07b1c97d33092bcf09d3d68203bb
sokintun7339656 <club_1303@mail.ru>
2022-03-03 19:00:29 +0500

    2-dars boldi

--- out/main.js
+++ out/main.js
@@ -4,13 +4,42 @@ Object.defineProperty(exports, "__esModule", { value: true });
 // console.log(name);
 // let dollar: number = 10740
 // console.log(dollar + 740);
-let x = 5;
-let y = 0;
-if (x > 0) {
-    y = Math.cos(x);
-    console.log("y=cos:", y);
+// let x: number = 5
+// let y: number = 0
+// if (x > 0) {
+//   y = Math.cos(x)
+//   console.log("y=cos:" , y);
+// }
+// else{
+//   y = Math.sin(x)
+//   console.log("y=sin:" , y);
+// }
+// console.log('y=', y);
+// let a: number = 5
+// let b: number = 2
+// if (a > b) {
+//   console.log('a katta');
+// }
+// else {
+//   console.log('b katta');
+// }
+// let km: number = 0.0125
+// let ft: number = 420
+// let kmm = km * 1000
+// let ftm = ft * 0.305
+// if (kmm > ftm) {
+//   console.log('km katta');
+// }
+// else {
+//   console.log('ftm katta');
+// }
+let a = 20;
+let b = 30;
+// let aa = a * 
+let bb = b * 3.6;
+if (a > bb) {
+    console.log('a katta');
 }
 else {
-    y = Math.sin(x);
-    console.log("y=sin:", y);
+    console.log('b katta');
 }
--- src/main.ts
+++ src/main.ts
@@ -4,20 +4,66 @@
 
 // let dollar: number = 10740
 // console.log(dollar + 740);
-let x: number = 5
-let y: number = 0
-if (x > 0) {
-  y = Math.cos(x)
-  console.log("y=cos:" , y);
+// let x: number = 5
+// let y: number = 0
+// if (x > 0) {
+//   y = Math.cos(x)
+//   console.log("y=cos:" , y);
   
-}
-else{
-  y = Math.sin(x)
-  console.log("y=sin:" , y);
-}
+// }
+// else{
+//   y = Math.sin(x)
+//   console.log("y=sin:" , y);
+// }
 
 
 // console.log('y=', y);
+
+
+// let a: number = 5
+// let b: number = 2
+
+// if (a > b) {
+//   console.log('a katta');
+  
+// }
+// else {
+//   console.log('b katta');
+  
+// }
+
+// let km: number = 0.0125
+// let ft: number = 420
+
+// let kmm = km * 1000
+// let ftm = ft * 0.305
+
+// if (kmm > ftm) {
+//   console.log('km katta');
+  
+// }
+// else {
+//   console.log('ftm katta');
+  
+// }
+
+// let a: number = 20
+// let b: number = 30
+
+// // let aa = a * 
+// let bb = b * 3.6
+
+// if (a > bb) {
+//   console.log('a katta');
+  
+// }
+// else {
+//   console.log('b katta');
+  
+// }
+
+
+
 
 
 
